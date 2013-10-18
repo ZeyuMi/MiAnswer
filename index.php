58c2789845efc70475f5b71b9acf3ee313caacb5
root <root@zeyu-OptiPlex-790.(none)>
2013-10-18 13:07:36 +0800

    tiny fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('SERVER_ROOT', dirname(__FILE__));
+define('SERVER_ROOT',dirname(__FILE__));
 define('DS', DIRECTORY_SEPARATOR);
 
 require_once(SERVER_ROOT . DS . 'library' . DS .  'bootstrap.php');
